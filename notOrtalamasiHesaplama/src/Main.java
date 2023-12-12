585ad7a70401ead6fb5e2d21536271036b815eb9
yigitortu <yigittortu287@gmail.com>
2023-12-12 13:31:09 +0300

    Not ortalamasina gore sinifi gecti ya da sinifta kaldi

--- notOrtalamasiHesaplama/src/Main.java
+++ notOrtalamasiHesaplama/src/Main.java
@@ -1,8 +1,39 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+    // Degiskenleri olustur
+    int mat, fizik, kimya, turkce, tarih, muzik;
 
+    //Scanner sınıfımızı tanımladık
+    Scanner inp = new Scanner(System.in);
 
+    //Kullanıcıdan değeri al
+        System.out.print("Matematik notunuz : ");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik notunuz : ");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya notunuz : ");
+        kimya = inp.nextInt();
+
+        System.out.print("Türkçe notunuz : ");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih notunuz : ");
+        tarih = inp.nextInt();
+
+        System.out.print("Müzik notunuz : ");
+        muzik = inp.nextInt();
+
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double ortalama = toplam / 6.0;
+        System.out.println("Ortalamanız : " + ortalama);
+
+        String kosul = ortalama > 60 ? "Sınıfı başarıyla geçtiniz" : "Sınıfta kaldınız";
+
+        System.out.print("Mevcut durumunuz : " + kosul);
     }
 }
