75deaf0c3336dfab5e8c5f2cb934914094c5b857
yigitortu <yigittortu287@gmail.com>
2023-12-12 19:21:41 +0300

    daireAlanCevreHesaplama bitirildi.

--- daireAlanCevreHesaplama/src/Main.java
+++ daireAlanCevreHesaplama/src/Main.java
@@ -1,7 +1,24 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Degiskenleri tanımlayalım
+        int r;
+        double pi = 3.14;
+        double alpha;
+
+        Scanner input = new Scanner(System.in); // Kullanıcıdan veri alma tanımlandı.
+
+        System.out.print("Yarı çap giriniz : ");
+        r = input.nextInt();
+        System.out.print("Merkez açı ölçüsünü giriniz : ");
+        alpha = input.nextDouble();;
+
+        double formul = (pi * (r*r)* alpha ) / 360;
+
+        System.out.println("Daire dilimini alanı : " + formul);
+
+
 
     }
 }
