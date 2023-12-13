6c6519a98fff1c7440cc233a75f08f8908b66b54
yigitortu <yigittortu287@gmail.com>
2023-12-13 09:34:16 +0300

    vücutKitleIndeks hesaplama programı tamamlandı.

--- vucutKitleEndeksHesaplama/src/Main.java
+++ vucutKitleEndeksHesaplama/src/Main.java
@@ -1,8 +1,19 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        // Degiskenleri tanımlayalım
+        double kilo, boy;
 
+        Scanner inp = new Scanner(System.in); // Scanner tanımlandı
 
+        System.out.print("Lütfen boyunuzu (m) türünde giriniz : "); // Kullanıcıdan boy istenildi
+        boy = inp.nextDouble();
+        System.out.print("Lütfen kilonuzu (kg) türünde giriniz : ");    // Kullanıcıdan kilo istenildi
+        kilo = inp.nextDouble();
+
+         double vucutKitle = kilo / (boy*boy); // Kitle indeksi formulu
+
+        System.out.println("Vücut kitle İndeksiniz : " + vucutKitle); // Son çıktı
 
     }
 }
